e283533bebb1f10b5002874b4aea36be30dad4ee
Oscar Fuentes <ofuentese@gmail.com>
2021-10-22 11:22:59 -0600

    fixing long task aligment

--- src/components/taskList.js
+++ src/components/taskList.js
@@ -5,7 +5,7 @@ function TaskList () {
     <div className='task-list'>
       <Task labelTask='Primera Tarea' />
       <Task labelTask='Segunda Tarea' />
-      <Task labelTask='Tercera Tarea' />
+      <Task labelTask='Tercera Tarea: configurar y acomodar los labels de los checkboxes, se ven muy feos y no son estéticos' />
       <button className='clearCompletedBtn'>Delete All Done</button>
     </div>
   )
